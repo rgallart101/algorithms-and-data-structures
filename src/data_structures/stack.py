3425899f158408eb6a6d91aef189714ef7da570c
Ramon Maria Gallart <rgallart@ramagaes.com>
2023-11-25 12:05:56 -0800

    Moved all implementations of Stack under

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -17,3 +17,23 @@ class LinkedListStack:
 
     def size(self):
         return self._data.size()
+
+
+class NativeStack:
+    def __init__(self) -> None:
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[self.size() - 1]
+
+    def size(self):
+        return len(self.items)
